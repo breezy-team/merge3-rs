968194d5c487f9e601df5a062621758d3615464c
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-04-14 16:49:34 +0100

    Add compare_range

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ fn intersect(ra: (i32, i32), rb: (i32, i32)) -> Option<(i32, i32)> {
 }
 
 #[cfg(test)]
-mod tests {
+mod intersect_tests {
     use super::intersect;
 
     #[test]
@@ -21,5 +21,40 @@ mod tests {
         assert_eq!(intersect((0, 10), (10, 15)), None);
         assert_eq!(intersect((0, 9), (10, 15)), None);
         assert_eq!(intersect((0, 9), (7, 15)), Some((7, 9)));
+    }
+}
+
+/// Compare a[astart:aend] == b[bstart:bend], without slicing.
+fn compare_range(
+    a: &[i32],
+    astart: usize,
+    aend: usize,
+    b: &[i32],
+    bstart: usize,
+    bend: usize,
+) -> bool {
+    if (aend - astart) != (bend - bstart) {
+        return false;
+    }
+    for (ia, ib) in (astart..aend).zip(bstart..bend) {
+        if a[ia] != b[ib] {
+            return false;
+        }
+    }
+    true
+}
+
+#[cfg(test)]
+mod compare_range_tests {
+    use super::compare_range;
+
+    #[test]
+    fn test_compare_range() {
+        let a = [1, 2, 3, 4, 5];
+        let b = [1, 2, 3, 4, 5];
+        assert!(compare_range(&a, 0, 5, &b, 0, 5));
+        assert!(compare_range(&a, 0, 3, &b, 0, 3));
+        assert!(!compare_range(&a, 0, 3, &b, 0, 4));
+        assert!(!compare_range(&a, 0, 3, &b, 1, 4));
     }
 }
